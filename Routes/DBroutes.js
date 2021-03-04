fd37039a9fb3c731d209fe5a48b0cb109eea7d7e
ErezKatsman <erezkatsman2311@gmail.com>
2021-03-04 16:38:26 +0200

    i created new brunch
    stated working on GET /:id method

--- Routes/DBroutes.js
+++ Routes/DBroutes.js
@@ -5,15 +5,15 @@ const bodyParser = require("body-parser");
 const DB = new DataBase();
 router.use(bodyParser.json());
 
-// router.get("/:id", (req, res) => {
-//   const { id } = req.params;
-//   DB.getFromData();
-//   DB.dataURL.forEach((url) => {
-//     if (url.shortURLid === id) {
-//       res.status(302).redirect(url.originalURL);
-//     }
-//   });
-// });
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  DB.getFromData();
+  DB.dataURL.forEach((url) => {
+    if (url.shortURLid === id) {
+      res.status(302).redirect(url.originalURL);
+    }
+  });
+});
 
 router.post("/", (req, res) => {
   const { url } = req.body;

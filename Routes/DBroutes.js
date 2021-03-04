39a6ee855cd85632a2151218b41499f7e165bd7e
ErezKatsman <erezkatsman2311@gmail.com>
2021-03-04 17:19:34 +0200

    finished GET method

--- Routes/DBroutes.js
+++ Routes/DBroutes.js
@@ -2,23 +2,28 @@ const express = require("express");
 const router = express.Router();
 const DataBase = require("../Classes/DBclass");
 const bodyParser = require("body-parser");
+const { json } = require("body-parser");
 const DB = new DataBase();
 router.use(bodyParser.json());
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
-  DB.getFromData();
-  DB.dataURL.forEach((url) => {
-    if (url.shortURLid === id) {
-      res.status(302).redirect(url.originalURL);
-    }
+  DB.loadData().then(() => {
+    DB.dataURL.forEach((url) => {
+      if (JSON.stringify(url.shortURLid) == id) {
+        return res.status(302).redirect(url.originalURL);
+      }
+    });
+    res
+      .status(404)
+      .json({ success: false, method: "POST", meassage: "Invalid url adress" });
   });
 });
 
 router.post("/", (req, res) => {
   const { url } = req.body;
   DB.addUrl(url).then((isAdded) => {
-    res.status(200).json({ success: true, method: "POST", AddedURL: isAdded });
+    res.status(200).json({ success: true, method: "POST", meassage: isAdded });
   });
 });
 
